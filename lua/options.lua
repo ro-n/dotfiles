ee3546f128bbedd26b7e85692fa8b3846d0e64bc
瀧 <66688118+ro-n@users.noreply.github.com>
2022-03-25 21:53:56 +0800

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,9 @@
 vim.o.mouse = 'a'
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 
 vim.wo.number = true
 vim.wo.relativenumber = true
